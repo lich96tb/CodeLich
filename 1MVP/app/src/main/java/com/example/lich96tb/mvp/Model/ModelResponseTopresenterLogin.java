90f7e8e20398a427922141174532bc22498f999b
lichdt <lich96tb@gmail.com>
2018-04-19 00:49:09 +0700

    thay doi

---
+++ 1MVP/app/src/main/java/com/example/lich96tb/mvp/Model/ModelResponseTopresenterLogin.java
@@ -0,0 +1,10 @@
+package com.example.lich96tb.mvp.Model;
+
+/**
+ * Created by LICH96TB on 4/19/2018.
+ */
+
+public interface ModelResponseTopresenterLogin {
+    void loginSuccess();
+    void loginFailed();
+}
---
+++ 1MVP/app/src/main/java/com/example/lich96tb/mvp/presenter/PresenterLoginResponseToView.java
@@ -0,0 +1,10 @@
+package com.example.lich96tb.mvp.presenter;
+
+/**
+ * Created by LICH96TB on 4/19/2018.
+ */
+
+public interface PresenterLoginResponseToView {
+    void onLoginSuccess();
+    void onloginFailed();
+}
