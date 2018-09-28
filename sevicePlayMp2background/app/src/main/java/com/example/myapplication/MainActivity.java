a98e45ea618c1d175e843695f64c77517a824086
lichdt <lich96tb@gmail.com>
2018-09-28 14:56:13 +0700

    ok

---
+++ sevicePlayMp2background/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RemoteViews;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click(View view) {
+        startService(new Intent(this,MyService.class));
+    }
+
+    public void stopmp3(View view) {
+        stopService(new Intent(this,MyService.class));
+    }
+}
---
+++ sevicePlayMp2background/app/src/main/java/com/example/myapplication/MyService.java
@@ -0,0 +1,100 @@
+package com.example.myapplication;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RemoteViews;
+
+public class MyService extends Service {
+    MediaPlayer mediaPlayer;
+    int NOTIFICATION_ID = 101;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.uocgi);
+        createNotification(this, "a", "b");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+    }
+
+
+    private NotificationManager mNotificationManager;
+    private NotificationCompat.Builder mBuilder;
+    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+
+    public void createNotification(Context mContext, String title, String message) {
+        Intent resultIntent = new Intent(mContext, MainActivity.class);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(mContext,
+                0 /* Request code */, resultIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder = new NotificationCompat.Builder(mContext);
+        RemoteViews contentView = new RemoteViews(mContext.getPackageName(), R.layout.notification);
+        mBuilder.setCustomContentView(contentView);
+        mBuilder.setSmallIcon(R.drawable.logo);
+        mBuilder.setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(false)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setContentIntent(resultPendingIntent);
+
+        mNotificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            assert mNotificationManager != null;
+            mBuilder.setChannelId(NOTIFICATION_CHANNEL_ID);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        Intent button_intent = new Intent("button");
+        button_intent.putExtra("id", "lichddd");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 123, button_intent, 0);
+        contentView.setOnClickPendingIntent(R.id.btn, pendingIntent);
+        Intent intent2 = new Intent("next");
+        intent2.putExtra("next", "nextfff");
+        PendingIntent pendingIntenta = PendingIntent.getBroadcast(this, 123, intent2, 0);
+        contentView.setOnClickPendingIntent(R.id.btnNext, pendingIntenta);
+
+        Notification notification=mBuilder.build();
+        startForeground(1, notification);
+    }
+}
