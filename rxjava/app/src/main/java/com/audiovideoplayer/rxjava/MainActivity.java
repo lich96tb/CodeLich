70d05adfc56e38f821edfdb53e8ab32110d1c912
lich96tb <lich96tb@gmail.com>
2019-05-18 11:38:58 +0700

    OK

---
+++ rxjava/app/src/main/java/com/audiovideoplayer/rxjava/MainActivity.java
@@ -0,0 +1,226 @@
+package com.audiovideoplayer.rxjava;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Observable<String> observable;
+    private Observer<String> myObs.2ervable;
+    private String greeting = "hello from";
+    private Disposable disposable;
+    Integer[] integers = {1, 2, 3};
+
+
+    private Observable<Integer> getANumberObservable;
+
+
+    @SuppressLint("CheckResult")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //funJust();
+        // funFrom();
+        //funDefer();
+        //funDeferChec();
+        //funDefer();
+        //funInterval();
+        //funCreat();
+        funAFreeThreaded();
+
+    }
+
+
+    private void funCreat() {
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) {
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onNext(4);
+                emitter.onNext(5);
+                emitter.onComplete();
+            }
+        }).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.e("onNext", "" + integer);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    private void funInterval() {
+        Observable.interval(1, TimeUnit.SECONDS).subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                Log.e("ASDdd", " " + aLong);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    private String movie;
+
+    @SuppressLint("CheckResult")
+    private void funDefer() {
+        movie = "Captain America: Civil War";
+        Observable<String> observable = Observable.defer(new Callable<ObservableSource<? extends String>>() {
+            @Override
+            public ObservableSource<? extends String> call() {
+                return Observable.just(movie);
+            }
+        });
+        movie = "do trong lich";
+        movie = "do trong lich1";
+        movie = "do trong lich2";
+        observable.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.e("AAAAAADDDd", "  " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+
+    }
+
+    private void funDeferChec() {
+        String movie = "Captain America: Civil War";
+        Observable<String> movieObservable = Observable.just(movie);
+        movie = "Batman v Superman: Dawn of Justice";
+        movieObservable.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.e("AAAAAAA ", "" + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    private void funFrom() {
+        Observable.fromArray(integers).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer integeraa) {
+                Log.e("AAAAAAA ", "" + Arrays.toString(new Integer[]{integeraa}));
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+    }
+
+    private void funJust() {
+        Observable.just(integers).subscribe(new Observer<Integer[]>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Integer[] integers) {
+                Log.e("AAAAAAA ", "" + Arrays.toString(integers));
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
+    }
+}
