b179d16ffb298f68f437b050e7acf7fd598d736e
lich96tb <lich96tb@gmail.com>
2019-08-13 01:43:12 +0700

    O

--- motionLayout/motionLayout/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ motionLayout/motionLayout/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.ImageFilterView.ImageFilterView
 import com.example.myapplication.ImageFilterView.ImageOneFilterView
+import com.example.myapplication.doi_vi_tri.DoiV
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentMain,ImageOneFilterView()).commit()
-       // supportFragmentManager.beginTransaction().replace(R.id.fragmentMain,ImageFilterView()).commit()
+      supportFragmentManager.beginTransaction().replace(R.id.fragmentMain,DoiV()).commit()
+    //  supportFragmentManager.beginTransaction().replace(R.id.fragmentMain,ImageOneFilterView()).commit()
+     // supportFragmentManager.beginTransaction().replace(R.id.fragmentMain,ImageFilterView()).commit()
     }
 }
